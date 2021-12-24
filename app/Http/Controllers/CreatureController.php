010baed8af584371d16f38a3b6649e24903d5e30
Conan <conan.spithoven@gmail.com>
2021-12-24 18:45:54 +0100

    added crud system

---
+++ app/Http/Controllers/CreatureController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Creature;
+use Illuminate\Http\Request;
+
+class CreatureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Creature::latest()->paginate(5);
+    
+        return view('Creatures.index',compact('data'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('Creatures.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+         $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+    
+        Creature::create($request->all());
+     
+        return redirect()->route('Creatures.index')
+                        ->with('success','Creature created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Creature  $creature
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Creature $creature)
+    {
+        return view('Creatures.show',compact('Creature'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Creature  $creature
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Creature $creature)
+    {
+        return view('Creatures.edit',compact('Creature'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Creature  $creature
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Creature $creature)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+    
+        $post->update($request->all());
+    
+        return redirect()->route('Creatures.index')
+                        ->with('success','Creature updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Creature  $creature
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Creature $creature)
+    {
+        $post->delete();
+    
+        return redirect()->route('Creatures.index')
+                        ->with('success','Creature deleted successfully');
+    }
+}
