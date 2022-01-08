85bea520ad1e80cbc3f97e59fffaa56ac36d8d1c
Conan <conan.spithoven@gmail.com>
2022-01-08 13:23:01 +0100

    cs: CRUD, inbetween commit for standard pages

---
+++ resources/views/pages/creatures.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.sidebar')
+@section('content')
+    <h1>Creatures</h1>
+    <p>Here will follow a list of all the creatures that have been posted and are currently active</p>
+@stop
